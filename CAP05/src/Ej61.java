afb4c99942cdd2c49881fc3d0bb338687084845b
adribarrios05 <abamar0501@g.educaand.es>
2023-11-08 22:24:55 +0100

    8/11/2023_v3

--- CAP05/src/Ej61.java
+++ CAP05/src/Ej61.java
@@ -1,27 +1,32 @@
 public class Ej61 {
-    public static void piramide(int altura){
-        for(int i=altura;i>=1;i--){
-            for(int j=1;j<=(altura-i);j++){
-                System.out.print(" ");
-            }
-            if (i>1) {
-                System.out.print("***");
-            }
-            String relleno = (i==1)?("*****"):(i==altura+1)?("***"):(" ");
-            if (i>1){
-                for(int j=2;j<=2*i-2;j++){
-                    System.out.print(relleno);
+    public static void escribirV(int altura){
+        if (altura%2==0) 
+            System.out.println("La altura debe ser impar");
+        else{
+            for(int i=altura;i>=1;i--){
+                String relleno = (i==1)?("*****"):(i==altura+1)?("***"):(" ");
+                for(int j=1;j<=(altura-i);j++){
+                    System.out.print(" ");
                 }
-                System.out.print("***");
+                if (i>1) {
+                    System.out.print("***");
+                }
+                
+                if (i>1){
+                    for(int j=2;j<=2*i-2;j++){
+                        System.out.print(relleno);
+                    }
+                    System.out.print("***");
+                }
+                else if (i==1)
+                    System.out.print("*****");
+                System.out.println("");
             }
-            else if (i==1)
-                System.out.print("*****");
-            System.out.println("");
         }
     }
     public static void main(String[] args) {
         System.out.print("Introduce la altura de la V: ");
         int altura = Integer.parseInt(System.console().readLine());
-        piramide(altura);
+        escribirV(altura);
     }
 }
